5eca27bfa274baf934ee93c28e7f29b90b97dd26
Ken-768 <kcp.is.revolutionary@gmail.com>
2023-09-03 13:12:51 +0900

    コンポーネントの基本

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/Hello.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Vue 3 入門</h1>
+  <Hello message="propsの使い方" />
 </template>
 
 <style scoped>
---
+++ src/components/Hello.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import {ref, reactive} from 'vue';
+
+  const count = ref(0);
+  const state = reactive({
+    count: 1,
+  });
+
+  const addRefCount = () => {
+    count.value++;
+  };
+
+  const addReactiveCount = () => {
+    state.count++;
+  }
+</script>
+
+<template>
+  <h2>初めてのコンポーネント</h2>
+  <p>Ref count: {{ count }}</p>
+  <p>Reactive count: {{ state.count }}</p>
+  <div>
+    <button @click="addRefCount">RefCount+</button>
+    <button @click="addReactiveCount">ReactiveCount+</button>
+  </div>
+</template>
